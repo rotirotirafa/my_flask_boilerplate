903dc7a36f1d662efd3fb5ec67d7ee91a037d3c9
rafael rotiroti <rotirotirafa@gmail.com>
2022-03-09 21:12:09 -0300

    config dinamica de ambiente

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM ubuntu:18.04
+FROM ubuntu:20.04
 
-RUN apt-get update -y && apt-get install -y python-pip python-dev libpq-dev
+RUN apt-get update -y && apt-get install && apt-get upgrade -y python-pip python-dev libpq-dev
 
 # We copy just the requirements.txt first to leverage Docker cache
 COPY ./requirements.txt /app/requirements.txt
--- src/app.py
+++ src/app.py
@@ -1,16 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-from src.settings import HOST, PORT, APP_SETTINGS
+from src.settings import APP_SETTINGS
 from src.utils.urls import build_urls
 
 
 def create_app():
     app = Flask(__name__)
-
-    app.config['ENV'] = 'development'
-    app.config['DEBUG'] = True
-
+    app.config.from_object(APP_SETTINGS)
     from src.models import tables
     # db.init_app(app)
     # with app.app_context():
